b5b68e2f6b0f7147bcf8ffdd2c72c3f51c37d064
gangadhar195 <gangadhars9545@gmail.com>
2025-02-20 12:04:25 +0530

    add style

--- react-hooks/src/component/ColorPicker/ColorPicker.jsx
+++ react-hooks/src/component/ColorPicker/ColorPicker.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 
 const ColorPicker = () => {
-    const[color,setColor]=useState("");
+    const[color,setColor]=useState("black");
 function handlecolorChange(e){
 setColor(e.target.value);
 }
   return (
     <div className='color-picker-container'>
-        <h1 className='color-display font-bold text-[30px]' style={{backgroundColor:color}}>Color Picker</h1>
-      <h1 >Color : {color}</h1>
-      <label>Set Color</label>
-      <input type='color' value={color} onChange={handlecolorChange}/>
+        <h1 className='color-display font-bold text-[30px]    ' >Color Picker</h1>
+      <h1 className='h-[300px] w-[300px] justify-center items-center 'style={{backgroundColor:color}}>Color : {color}</h1>
+      <label className='font-bold'>Set Color</label>
+      <input type='color' value={color} onChange={handlecolorChange} className='mt-4 h-[50px] w-[90px]'/>
     </div>
   )
 }

48414d5b72ce7c11ca16da3349b37a428bef995e
gangadhar195 <gangadhars9545@gmail.com>
2025-02-21 19:08:39 +0530

    reducer

--- react-hooks/src/component/Hooks/Hooks.jsx
+++ react-hooks/src/component/Hooks/Hooks.jsx
@@ -1,11 +1,18 @@
-import React, { useEffect, useState, useRef, useMemo, useCallback, useReducer } from "react";
-import Header from "./Header";
+import React, {
+  useEffect,
+  useState,
+  useRef,
+  useMemo,
+  useCallback,
+  useReducer,
+} from "react";
+// import Header from "./Header";
 
 const Hooks = () => {
-//   const [count, setCount] = useState(0);
-//     const[number,setNumber]=useState(0);
-//   // const[ren,setRen]=useState(0);
-//   const ren = useRef();
+  //   const [count, setCount] = useState(0);
+  //     const[number,setNumber]=useState(0);
+  //   // const[ren,setRen]=useState(0);
+  //   const ren = useRef();
   //useREF Hooks
   // useEffect(()=>{
   //     // setRen(prev=>prev+1);
@@ -17,36 +24,38 @@ const Hooks = () => {
   //     ren.current.style.width="350px";
 
   // }
-  
-//   function cubeNum(){
-//     console.log('Calculation Done');
-    
-//     return Math.pow(number,3);
-//   }
 
-//   const result = useMemo(()=>cubeNum(number),[number])
+  //   function cubeNum(){
+  //     console.log('Calculation Done');
 
-// const UseCallBack = useCallback(()=>{
-    
-// },[])
+  //     return Math.pow(number,3);
+  //   }
 
-//UseReducer
-const initialSate = {count:0}
-const reducer = (state,action)=>{
-    switch(action.type){
-        case 'Increse':{
-            return {count:state.count+1}
-        }
-        case 'Decrese':{
-            return {count:state.count-1}
-        }
-        default :{
-            return state;
-        }
+  //   const result = useMemo(()=>cubeNum(number),[number])
+
+  // const UseCallBack = useCallback(()=>{
+
+  // },[])
+
+  //UseReducer
+  const initialSate = { count: 0 };
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "Increse": {
+        return { count: state.count + 1 };
+      }
+      case "Decrese": {
+        return { count: state.count - 1 };
+      }
+      case "input": {
+        return { count: action.payload };
+      }
+      default: {
+        return state;
+      }
     }
-   
-}
-const [state,dispatch] = useReducer(reducer,initialSate)
+  };
+  const [state, dispatch] = useReducer(reducer, initialSate);
 
   return (
     <div className="flex flex-col justify-center items-center mt-5 ">
@@ -59,7 +68,7 @@ const [state,dispatch] = useReducer(reducer,initialSate)
       <button className='bg-sky-500 hover:bg-sky-700 p-2'onClick={()=>{setCount(prev=>prev-1)}} >-1</button> */}
       {/* <input type='text' placeholder='Input' className='border-2 pl-2 mb-2'ref={ren}/>
       <button className='bg-sky-500 hover:bg-sky-700 p-2 'onClick={btnClick} >Click</button> */}
-     
+
       {/* <h1>useMemo Hook</h1>
       <input type="number" placeholder="" className="border-2 pl-2 mb-2" value={number} onChange={(e)=>setNumber(e.target.value)}/>
       <h1>Cube of the Number :{result} </h1>
@@ -82,18 +91,36 @@ const [state,dispatch] = useReducer(reducer,initialSate)
         -1
       </button> */}
 
-
       {/* <Header UseCallBack={UseCallBack}/>
       <div>
       <h1>{count}</h1>
       <button className='bg-sky-500 hover:bg-sky-700 p-2'onClick={()=>{setCount(prev=>prev+1)}} >+1</button>
       </div> */}
 
-      <div className="border-2 p-2">
-        <h1 className="text-center">Reducer</h1>
-      <h1 className="text-center">{state.count}</h1>
-      <button className='bg-sky-500 hover:bg-sky-700 p-2 mr-4'onClick={()=>dispatch({type:"Increse"})} >Increse</button>
-      <button className='bg-sky-500 hover:bg-sky-700 p-2'onClick={()=>dispatch({type:"Decrese"})} >Decrese</button> 
+      <div className=" flex flex-col border-2 p-2 ">
+        <h1 className="text-center">Use Reducer</h1>
+        <h1 className="text-center font-bold">{state.count}</h1>
+        <div className=" flex flex-row m-auto p-">
+          <button
+            className="bg-sky-500 hover:bg-green-700 text-white mr-2 p-2"
+            onClick={() => dispatch({ type: "Increse" })}
+          >
+            Increse
+          </button>
+          <button
+            className="bg-sky-500 hover:bg-green-700 text-white p-2"
+            onClick={() => dispatch({ type: "Decrese" })}
+          >
+            Decrese
+          </button>
+        </div>
+
+        <input
+          className="text-center font-bold border-2 m-2"
+          value={state.count}
+         
+          onChange={(e) =>dispatch({type:'input', payload:Number(e.target.value)})}
+          type="number"/>
       </div>
     </div>
   );
